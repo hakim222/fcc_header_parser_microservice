2bbf8bd8903c349eb5be1c6102466bcc590ad788
Hakim 'Azizan <hazizan22@gmail.com>
2024-07-15 07:57:34 +0000

    feat: add solution for the header parser

--- index.js
+++ index.js
@@ -28,3 +28,10 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/whoami', (req,res)=>{
+  var ipaddress = req.ip;
+  var language = req.headers['accept-language'];
+  var software = req.headers['user-agent'];
+  res.json({ipaddress: ipaddress, language: language, software: software});
+})
